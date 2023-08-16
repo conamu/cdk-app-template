7d677946d3374930384187f38e6fe2996336121b
conamu <constantin.amundsen@ho-me.zone>
2023-08-17 01:30:35 +0200

    add build step as dependency

--- config/config.go
+++ config/config.go
@@ -3,6 +3,6 @@ package config
 import "github.com/spf13/viper"
 
 func Init() {
-	viper.SetDefault("aws-account", "000000000000")
+	viper.SetDefault("aws-account", "038796470268")
 	viper.SetDefault("aws-region", "eu-west-1")
 }
