da410f9b2f7005ef51e0526e0bed68c84742b7a0
conamu <constantin.amundsen@ho-me.zone>
2023-08-18 13:38:25 +0200

    update local ci

--- scripts/localstack-deploy.sh
+++ scripts/localstack-deploy.sh
@@ -4,6 +4,6 @@ export AWS_SECRET_ACCESS_KEY=test
 export CDK_DEPLOY_ACCOUNT=000000000000
 export CDK_DEPLOY_REGION=eu-west-1
 export AWS_DEFAULT_REGION=eu-west-1
-cdklocal synth
 cdklocal bootstrap -v
+cdklocal synth
 cdklocal deploy --require-approval never -v
